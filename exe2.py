7c70a35f6bc02f0af644f91110fc09941cf68339
mezermize <103459165+mezermize@users.noreply.github.com>
2023-03-01 22:40:47 +0000

    ex2 terminado

--- exe2.py
+++ exe2.py
@@ -2,22 +2,33 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def contaLetras(letras, texto):
-
-    count = {}.fromkeys(letras, 0)
+def countLetters(letras, texto):
+    count = {}.fromkeys(letras, 0)  # cria o hashmap
 
     for letras in texto:
         if letras in count:
             count[letras] += 1
-    return count
+    return count  # devolve o hashmap
 
 
 vogais = 'aeiou'
 file = open('poema.txt', 'r', encoding='utf-8')
 poem = file.read()
+# poem = "aaeeiou"
 poem.casefold()
 
-
 if __name__ == '__main__':
+    result = countLetters(vogais, poem)
 
-    print(contaLetras(vogais, poem))
+    print("O numero de cada vogal no texto é: " + str(result))
+
+    maxi = max(result.values())
+    result2 = ([k for k in result if result.get(k) == maxi])
+    if len(result2) > 1:
+        print("Há vários vencedores")
+        print("os varios vencedores são: " + str(result2))
+    else:
+        print("A vogal mais utilizada foi: '" + result2[0] + "'")
+
+
+
