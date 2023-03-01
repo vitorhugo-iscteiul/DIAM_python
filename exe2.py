8b2adb3e9e9a34275e18f37abeaf93e5cc8e5c2a
mezermize <103459165+mezermize@users.noreply.github.com>
2023-03-01 21:41:56 +0000

    o que ja tinha feito da aula

--- exe2.py
+++ exe2.py
@@ -2,14 +2,22 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def contaLetras(letras, texto):
 
+    count = {}.fromkeys(letras, 0)
+
+    for letras in texto:
+        if letras in count:
+            count[letras] += 1
+    return count
+
+
+vogais = 'aeiou'
 file = open('poema.txt', 'r', encoding='utf-8')
 poem = file.read()
-vogais = ['a', 'e', 'i', 'o', 'u']
-if __name__ == '__main__':
-    count = 0
-    for x in range(len(poem)):
-        if poem[x] in vogais:
-            count += 1
+poem.casefold()
 
-    print(count)
+
+if __name__ == '__main__':
+
+    print(contaLetras(vogais, poem))
